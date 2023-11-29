be371bf2ab1d503a033f16b1dac985b9dc43f4ba
Reece Yang <reeceyang@live.com>
2023-11-28 23:13:19 -0500

    1-2 character tokens

--- src/main.rs
+++ src/main.rs
@@ -3,40 +3,64 @@ use std::env;
 use std::io;
 use std::io::Write;
 
-fn run_prompt() {
-    loop {
-        print!("> ");
-        let _ = io::stdout().flush();
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(n) => {
-                if n == 0 {
-                    break;
+use scanner::Scanner;
+
+mod scanner;
+
+pub struct Lox {
+    had_error: bool,
+}
+
+impl Lox {
+    fn run_prompt(&mut self) {
+        loop {
+            print!("> ");
+            let _ = io::stdout().flush();
+            let mut input = String::new();
+            match io::stdin().read_line(&mut input) {
+                Ok(n) => {
+                    if n == 0 {
+                        break;
+                    }
+                    self.run(&input)
                 }
-                run(&input)
+                Err(error) => println!("error: {error}"),
             }
-            Err(error) => println!("error: {error}"),
         }
     }
-}
 
-fn run_file(file_path: &String) {
-    match fs::read_to_string(file_path) {
-        Ok(source) => run(&source),
-        Err(error) => println!("error: {error}")
+    fn run_file(&mut self, file_path: &String) {
+        match fs::read_to_string(file_path) {
+            Ok(source) => self.run(&source),
+            Err(error) => println!("error: {error}")
+        }
     }
-}
 
-fn run(source: &String) {
-    println!("{source}");
+    fn run(&mut self, source: &String) {
+        let mut scanner = Scanner::new(source, self);
+        let tokens = scanner.scan_tokens();
+        for token in tokens {
+            println!("{token}");
+        }
+    }
+
+    fn error(&mut self, line: i32, message: &str) {
+        self.report(line, message);
+    }
+
+    fn report(&mut self, line: i32, message: &str) {
+        eprintln!("[line {line}] Error: {message}");
+        self.had_error = true;
+    }
 }
 
 fn main() {
+    let mut lox = Lox {had_error: false };
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        run_prompt();
+        lox.run_prompt();
     } else {
         let file_path = &args[1];
-        run_file(file_path);
+        lox.run_file(file_path);
     }
 }
---
+++ src/scanner.rs
@@ -0,0 +1,233 @@
+use core::fmt;
+use std::string::String;
+
+use crate::Lox;
+
+#[derive(Debug)]
+enum TokenType {
+    // Single-character tokens.
+    LeftParen,
+    RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+
+    // Literals.
+    Identifier,
+    String,
+    Number,
+
+    // Keywords.
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+
+    Eof,
+}
+
+#[derive(Debug)]
+enum Literal {
+    Str(String),
+    F64(f64),
+}
+
+pub struct Token {
+    token_type: TokenType,
+    lexeme: String,
+    literal: Option<Literal>,
+    line: i32,
+}
+
+impl fmt::Display for Token {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "{:?} {} {:?}",
+            self.token_type, self.lexeme, self.literal
+        )
+    }
+}
+
+pub struct Scanner<'a> {
+    source: &'a String,
+    tokens: Vec<Token>,
+    start: usize,
+    current: usize,
+    line: i32,
+    lox: &'a mut Lox,
+}
+
+impl Scanner<'_> {
+    pub fn new<'a>(source: &'a String, lox: &'a mut Lox) -> Scanner<'a> {
+        Scanner {
+            source,
+            tokens: vec![],
+            start: 0,
+            current: 0,
+            line: 0, 
+            lox,
+        }
+    }
+
+    fn is_at_end(&self) -> bool {
+        self.current >= self.source.chars().count()
+    }
+
+    pub fn scan_tokens(&mut self) -> &Vec<Token> {
+        while !self.is_at_end() {
+            self.start = self.current;
+            self.scan_token();
+        }
+
+        self.tokens.push(Token {
+            token_type: TokenType::Eof,
+            lexeme: "".to_string(),
+            literal: None,
+            line: self.line,
+        });
+        &self.tokens
+    }
+
+    fn scan_token(&mut self) {
+        let c = self.advance();
+        match c {
+            '(' => self.add_token(TokenType::LeftParen, None),
+            ')' => self.add_token(TokenType::RightParen, None),
+            '{' => self.add_token(TokenType::LeftBrace, None),
+            '}' => self.add_token(TokenType::RightBrace, None),
+            ',' => self.add_token(TokenType::Comma, None),
+            '.' => self.add_token(TokenType::Dot, None),
+            '-' => self.add_token(TokenType::Minus, None),
+            '+' => self.add_token(TokenType::Plus, None),
+            ';' => self.add_token(TokenType::Semicolon, None),
+            '*' => self.add_token(TokenType::Star, None),
+            '!' => {
+                let token_type = if self.match_next('=') {
+                    TokenType::BangEqual
+                } else {
+                    TokenType::Bang
+                };
+                self.add_token(token_type, None)
+            }
+            '=' => {
+                let token_type = if self.match_next('=') {
+                    TokenType::EqualEqual
+                } else {
+                    TokenType::Equal
+                };
+                self.add_token(token_type, None)
+            }
+            '<' => {
+                let token_type = if self.match_next('=') {
+                    TokenType::LessEqual
+                } else {
+                    TokenType::Less
+                };
+                self.add_token(token_type, None)
+            }
+            '>' => {
+                let token_type = if self.match_next('=') {
+                    TokenType::GreaterEqual
+                } else {
+                    TokenType::Greater
+                };
+                self.add_token(token_type, None)
+            }
+            '/' => {
+              if self.match_next('/') {
+                while self.peek() != '\n' && !self.is_at_end() {
+                  self.advance();
+                }
+              } else {
+                self.add_token(TokenType::Slash, None)
+              }
+            }
+            ' ' | '\r' | '\t' => {}
+            '\n' => self.line += 1,
+            _ => self.lox.error(self.line, "Unexpected character."),
+        }
+    }
+
+    fn advance(&mut self) -> char {
+        let c = self.source.chars().nth(self.current);
+        self.current += 1;
+        match c {
+            Some(c) => c,
+            None => panic!("Scanner failed at line {}", self.line),
+        }
+    }
+
+    fn match_next(&mut self, expected: char) -> bool {
+        if self.is_at_end() {
+            return false;
+        }
+        let c = self.source.chars().nth(self.current);
+        match c {
+            Some(c) => {
+                if c != expected {
+                    false
+                } else {
+                    self.current += 1;
+                    true
+                }
+            }
+            None => panic!("Scanner failed at line {}", self.line),
+        }
+    }
+
+    fn peek(&self)  -> char{
+      if self.is_at_end() {'\0'} else {
+        let c = self.source.chars().nth(self.current);
+        match c {
+            Some(c) => {
+              c
+            }
+            None => panic!("Scanner failed at line {}", self.line),
+        }
+      }
+    }
+
+    fn add_token(&mut self, token_type: TokenType, literal: Option<Literal>) {
+        let text: String = self
+            .source
+            .chars()
+            .skip(self.start)
+            .take(self.current - self.start)
+            .collect();
+        self.tokens.push(Token {
+            token_type,
+            lexeme: text,
+            literal,
+            line: self.line,
+        })
+    }
+}
